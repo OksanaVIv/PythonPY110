17a8120206110e7ab385c12a3ed397d53bb01d99
Oksiva <oksana.iv.spb@mail.ru>
2023-12-28 20:30:21 +0300

    4 Работа с корзиной

--- store/views.py
+++ store/views.py
@@ -75,11 +75,29 @@ def products_view(request):
                                                                  'indent': 4})
 
 
+# def cart_view(request):
+#     if request.method == "GET":
+#         data = view_in_cart()  # TODO Вызвать ответственную за это действие функцию
+#         return JsonResponse(data, json_dumps_params={'ensure_ascii': False,
+#                                                      'indent': 4})
+
+
 def cart_view(request):
     if request.method == "GET":
-        data = view_in_cart()  # TODO Вызвать ответственную за это действие функцию
-        return JsonResponse(data, json_dumps_params={'ensure_ascii': False,
+        data = view_in_cart()
+        if request.GET.get('format') == 'JSON':
+            return JsonResponse(data, json_dumps_params={'ensure_ascii': False,
                                                      'indent': 4})
+        products = []  # Список продуктов
+        for product_id, quantity in data['products'].items():
+            product = DATABASE[str(product_id)]  # 1. Получите информацию о продукте из DATABASE по его product_id. product будет словарём
+            product["quantity"] = quantity
+            # 2. в словарь product под ключом "quantity" запишите текущее значение товара в корзине
+            product[
+                "price_total"] = f"{quantity * product['price_after']:.2f}"  # добавление общей цены позиции с ограничением в 2 знака
+            # 3. добавьте product в список products
+            products.append(product)
+        return render(request, "store/cart.html", context={"products": products})
 
 
 def cart_add_view(request, id_product):
