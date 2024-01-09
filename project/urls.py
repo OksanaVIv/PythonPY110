9c8e1c1a7006bb56f78ce4953c1697abb63084ee
OksanaVIv <oksana.iv.spb@mail.ru>
2024-01-09 22:52:34 +0300

    Реализовано: авторизация пользователя

--- project/urls.py
+++ project/urls.py
@@ -35,5 +35,6 @@ urlpatterns = [
     # path('weather/', weather_view),
     path('', include('app_weather.urls')),
     path('', include('store.urls')),
+    path('login/', include('app_login.urls')),
 
 ]
